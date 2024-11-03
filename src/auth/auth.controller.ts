635ee2afad0b7cf44d4aef4543c3b0ad08762808
DungPHPAndReact <dung2000cn@gmail.com>
2024-11-03 17:48:16 +0700

    connect DB and create logic register

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,9 +1,44 @@
 import { Request, Response } from 'express';
+import { User } from './auth.dto';
+import userModel from './auth.model';
+import bcrypt from 'bcrypt';
 
-export const authRegister = (req: Request, res: Response) => {
-    res.status(200).json({ message: 'Register success' });
-}
+export const authRegister = async (
+	req: Request,
+	res: Response
+): Promise<any> => {
+	try {
+		const user: User = req.body;
+		const { email, password, username } = user;
+
+		const existingUser = await userModel.findOne({
+			email,
+		});
+
+		if (existingUser) {
+			return res.status(404).json({
+				status: 'error',
+				message: 'Email đã được đăng ký',
+			});
+		}
+
+		const passwordHash = await bcrypt.hash(password, 10);
+
+		const newUser = new userModel({
+			email,
+			username,
+			password: passwordHash,
+			role: 'user',
+		});
+
+		await newUser.save();
+
+		res.status(201).json({ status: 'success', message: 'Đăng ký thành công' });
+	} catch (err: any) {
+        res.status(500).json({ status: 'error', message: 'Đã xảy ra vấn đề trong quá trình đăng ký' });
+    }
+};
 
 export const authLogin = (req: Request, res: Response) => {
-    res.status(200).json({ message: 'Login success' });
-}
+	res.status(200).json({ message: 'Login success' });
+};
--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,12 @@
 import express, { Request, Response, NextFunction } from 'express';
 import { authProtected, authToken, authLogin, authRegister } from './controllers/auth.controller';
 import { authenticateToken, authorization } from './middlewares/auth.middleware';
-
+import dotenv from 'dotenv';
 import authRouter from './auth/auth.route';
 import postRouter from './post/post.route';
+import mongoose from 'mongoose';
 
+dotenv.config();
 
 const app = express();
 app.use(express.json());
@@ -18,8 +20,20 @@ app.use('/', authRouter);
 app.use('/api/v1/post', postRouter);
 
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
+const DATABASE_URL = process.env.DATABASE_URL || "mongodb://127.0.0.1:27017/Post-Management";
+
+const connectDB = async() => {
+	try{
+		await mongoose.connect(DATABASE_URL)
+		console.log('Kết nối đến MongoDB thành công');
+	} catch (error){
+		console.log('Kết nối đến MongoDB thất bại');
+		console.log(error);
+	}
+}
 
 app.listen(PORT, () => {
+	connectDB();
 	console.log(`Server is running on port ${PORT}`);
 });
