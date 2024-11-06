06bc0b416cacd99277e59f8d618461d7f5a94122
DungPHPAndReact <dung2000cn@gmail.com>
2024-11-06 19:20:16 +0700

    init middleware authorization, middleware checkOwnership

--- src/post/post.route.ts
+++ src/post/post.route.ts
@@ -1,13 +1,23 @@
 import express from 'express';
-import { createPost, deletePost, readPost, readPosts, updatePost } from './post.controller';
-import { authenticateToken } from '../middlewares/auth.middleware';
+import {
+	authenticateToken,
+	authorization,
+    checkOwnership,
+} from '../middlewares/auth.middleware';
+import {
+	createPost,
+	deletePost,
+	readPost,
+	readPosts,
+	updatePost,
+} from './post.controller';
 
 const postRouter = express.Router();
 
 postRouter.get('/', readPosts);
 postRouter.get('/:id', readPost);
-postRouter.post('/',authenticateToken, createPost);
-postRouter.put('/:id',authenticateToken, updatePost);
-postRouter.delete('/:id',authenticateToken, deletePost);
+postRouter.post('/', authenticateToken, createPost);
+postRouter.put('/:id', authenticateToken, updatePost);
+postRouter.delete('/:id', authenticateToken, authorization,checkOwnership, deletePost);
 
 export default postRouter;
